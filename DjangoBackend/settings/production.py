b4c17be7e05390edcec5efb04c9a603f7ccaf6f6
Jarred Benham <j.benham@outlook.com>
2017-09-11 14:23:30 +1000

    Dev settings update

--- DjangoBackend/settings/production.py
+++ DjangoBackend/settings/production.py
@@ -6,3 +6,5 @@ DEBUG = False
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['vast-gorge-31543.herokuapp.com']
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
